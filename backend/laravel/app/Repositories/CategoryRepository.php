6eec06555c92353d4a8d272274900f42c10047b8
Pablo Roxo <pabloricardoroxosilva@gmail.com>
2023-09-29 00:12:02 -0300

    Update CategoryRepository

--- backend/laravel/app/Repositories/CategoryRepository.php
+++ backend/laravel/app/Repositories/CategoryRepository.php
@@ -10,40 +10,33 @@ class CategoryRepository extends Repository
 {
     public function index($data)
     {
-        $categories = Category::select()
-
-            // Filters
-            ->when(!empty($data['title']), function (&$query) use ($data) {
+        return Category::select()
+            ->when(!empty($data['title']), function ($query) use ($data) {
                 $query->where('title', 'like', '%' . $data['title'] . '%');
             })
-            ->when(!empty($data['description']), function (&$query) use ($data) {
+            ->when(!empty($data['description']), function ($query) use ($data) {
                 $query->where('description', 'like', '%' . $data['description'] . '%');
             })
-
-            // Ordering
-            ->when(!empty($data['orderBy']), function (&$query) use ($data) {
-                $order = (!empty($data['order']) && $data['order'] === 'desc')
-                    ? 'desc'
-                    : 'asc';
-                $query->orderBy($data['orderBy'], $order);
+            ->when(true, function ($query) use ($data) {
+                if (!empty($data['orderBy'])) {
+                    $orderBy = $data['orderBy'];
+                    $order = (!empty($data['order']) && $data['order'] === 'desc')
+                        ? 'desc'
+                        : 'asc';
+                } else {
+                    $orderBy = 'title';
+                    $order = 'asc';
+                }
+                $query->orderBy($orderBy, $order);
             })
-
             ->paginate();
-
-        return $categories;
     }
 
     public function show($id)
     {
-        $category = Category::select()
-            ->where('id', $id)
-            ->first();
-
-        if (!$category) {
+        return Category::findOr($id, function () {
             throw new Exception('Category not found.', Response::HTTP_NOT_FOUND);
-        }
-
-        return $category;
+        });
     }
 
     public function create($data)
@@ -55,6 +48,7 @@ class CategoryRepository extends Repository
     public function update($category, $data)
     {
         $category->fill($data);
+
         if ($category->isDirty()) {
             $category->update();
         }
